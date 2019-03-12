398362c63963de0919e924b5be0b49956c500f69
HazekiahWon <patrickwhz1022@gmail.com>
2019-03-12 12:38:01 +0800

    modify

--- Make_classifier_git.py
+++ Make_classifier_git.py
@@ -18,14 +18,14 @@ with tf.Graph().as_default():
 
     with tf.Session() as sess:
 
-        datadir = '/..Path to align face data../'
+        datadir = 'aligned'
         dataset = facenet.get_dataset(datadir)
         paths, labels = facenet.get_image_paths_and_labels(dataset)
         print('Number of classes: %d' % len(dataset))
         print('Number of images: %d' % len(paths))
 
         print('Loading feature extraction model')
-        modeldir = '/..Path to Pre-trained model../20170512-110547/20170512-110547.pb'
+        modeldir = 'model_data/20170512-110547.pb'
         facenet.load_model(modeldir)
 
         images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
@@ -35,7 +35,7 @@ with tf.Graph().as_default():
 
         # Run forward pass to calculate embeddings
         print('Calculating features for images')
-        batch_size = 1000
+        batch_size = 100
         image_size = 160
         nrof_images = len(paths)
         nrof_batches_per_epoch = int(math.ceil(1.0 * nrof_images / batch_size))
@@ -48,7 +48,7 @@ with tf.Graph().as_default():
             feed_dict = {images_placeholder: images, phase_train_placeholder: False}
             emb_array[start_index:end_index, :] = sess.run(embeddings, feed_dict=feed_dict)
 
-        classifier_filename = '/..Path to save classifier../my_classifier.pkl'
+        classifier_filename = 'classifiers/my_classifier.pkl'
         classifier_filename_exp = os.path.expanduser(classifier_filename)
 
         # Train classifier
